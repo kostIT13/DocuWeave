7f42124c5178a3005418ffd8f242e914057fd22d
Konstantin Balandin <kostabalandin169@gmail.com>
2026-04-23 13:56:56 +0700

    fix get settings

--- backend/src/infrastructure/core/config.py
+++ backend/src/infrastructure/core/config.py
@@ -8,28 +8,28 @@ class Settings(BaseSettings):
 
     DEBUG: bool = False
 
-    CHROMA_HOST: str 
-    CHROMA_PORT: str 
-    CHROMA_COLLECSTIONS: str 
+    # CHROMA_HOST: str 
+    # CHROMA_PORT: str 
+    # CHROMA_COLLECSTIONS: str 
 
-    OLLAMA_HOST: str 
-    OLLAMA_EMBBEDING_MODEL: str 
-    OLLAMA_LLM: str
+    # OLLAMA_HOST: str 
+    # OLLAMA_EMBBEDING_MODEL: str 
+    # OLLAMA_LLM: str
     
-    OPENAI_API_KEY: str | None = None
-    PROJECT_NAME: str = "DocuWeave"
+    # OPENAI_API_KEY: str | None = None
+    # PROJECT_NAME: str = "DocuWeave"
 
-    ENVIRONMENT: str = "developing"
+    # ENVIRONMENT: str = "developing"
 
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    # SECRET_KEY: str
+    # ALGORITHM: str = "HS256"
+    # ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False, extra="ignore")
 
 
-@lru_cache()
-async def get_settings() -> Settings:
+@lru_cache
+def get_settings() -> Settings:
     return Settings()
 
 settings = get_settings()
