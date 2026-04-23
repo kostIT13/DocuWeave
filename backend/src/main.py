97241ffcc54fc7aaa825449e722e701d18d5d785
Konstantin Balandin <kostabalandin169@gmail.com>
2026-04-23 13:58:40 +0700

    add models in main

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from src.lifespan import lifespan
 from src.infrastructure.core.logging_settings import setup_logging
 from src.infrastructure.core.config import settings
+from src.infrastructure.models.chat_session import ChatSession
+from src.infrastructure.models.document import Document
+from src.infrastructure.models.graph_trace import GraphTrace
+from src.infrastructure.models.message import Message 
+from src.infrastructure.models.project import Project
+from src.infrastructure.models.user import User
 
 
 setup_logging(level=settings.LOG_LEVEL)
