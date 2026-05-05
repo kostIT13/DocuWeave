e74244af2458ad4635c4742297915f6ba516bcef
Konstantin Balandin <kostabalandin169@gmail.com>
2026-05-05 21:27:42 +0700

    fix auth

--- backend/src/api/auth/dependencies.py
+++ backend/src/api/auth/dependencies.py
@@ -15,7 +15,7 @@ async def get_user_service(db: AsyncSession = Depends(get_db)):
 UserServiceDependency = Annotated[UserService, Depends(get_user_service)]
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 
 async def get_current_user(
